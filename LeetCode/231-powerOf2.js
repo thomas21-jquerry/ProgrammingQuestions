89c9bf5b2867645c5246b81411f5d8b27661582d
ThomasJose1999 <thomas.jose@phaethon.tech>
2023-10-21 23:07:11 +0530

    day 4✅

---
+++ LeetCode/231-powerOf2.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfTwo = function(n) {
+    if(n===0){
+        return 0;
+    }
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+};
