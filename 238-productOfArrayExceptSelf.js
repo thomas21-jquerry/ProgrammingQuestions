2439f922951302af3405f7fa9f854d8a76e2fe5c
thomas21-jquerry <thomasjose875@gmail.com>
2024-10-29 22:56:21 +0530

    238 done ✅

---
+++ 238-productOfArrayExceptSelf.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var productExceptSelf = function(nums) {
+    let isZeroExist = false;
+    let isMultZeroExist = false;
+    let mul = 1
+    let ans = []
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i]!=0){
+            mul=mul*nums[i]
+        }
+        else{
+            if(!isZeroExist){
+                isZeroExist = true;
+            }
+            else{
+                isMultZeroExist = true;
+                break
+            }
+            
+        }
+        
+    }
+
+    for(let i = 0;i<nums.length;i++){
+        if(isMultZeroExist){
+            ans.push(0);
+            continue;
+        }
+        if(isZeroExist){
+            if(nums[i]==0){
+                ans.push(mul)
+            }
+            else{
+                ans.push(0)
+            }
+               
+        }
+        else {
+            ans.push(mul/nums[i])
+        }
+        
+    }
+    return ans
+};
