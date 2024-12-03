3c3af51916d00a801c7d72b61e29fe9a0ad93ad6
thomas21-jquerry <thomasjose875@gmail.com>
2024-12-03 19:18:53 +0530

    3 redo

--- LeetCode/3-longestSubstringWithoutRepeating.js
+++ LeetCode/3-longestSubstringWithoutRepeating.js
@@ -3,23 +3,17 @@
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-    let ans = 0;
-    let cur = 0;
-    let sub = "";
-    for(let i = 0;i<s.length;i++){ 
-        if(sub.indexOf([s[i]]) === -1){
-            sub+=s[i];
-            cur+=1;
-            ans = Math.max(ans,cur);
-            
-        }
-        else{
-            i = sub.indexOf(s[i]) + 1
-            cur = 1;
-            sub = s[i];
-        }
+    let n = s.length;
+    let maxLength = 0;
+    let lastIndex = new Map();
+    
+    let start = 0;
+    for (let end = 0; end < n; end++) {
+        let currentChar = s[end];
+        start = Math.max(start, lastIndex.get(currentChar) || 0);
+        maxLength = Math.max(maxLength, end - start + 1);
+        lastIndex.set(currentChar, end + 1);
     }
-    return ans;
+    
+    return maxLength;
 };
-
-console.log("ans",lengthOfLongestSubstring("abcabcbb"))
