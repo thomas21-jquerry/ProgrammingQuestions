239b120a7019d187aeaafb29da857ed29b3857eb
thomas21-jquerry <thomasjose875@gmail.com>
2025-02-26 14:00:18 +0530

    done

--- LeetCode/79-wordSearch.js
+++ LeetCode/79-wordSearch.js
@@ -31,3 +31,31 @@ var exist = function(board, word) {
     }
     return false;
 };
+
+
+var exist = function(board, word) {
+    const row = board.length;
+    const col = board[0].length;
+    function helper(r,c,k){
+     if(k == word.length){
+         return true
+     }
+     if(r<0 || c < 0 || r>=row || c>= col || word.charAt(k)!=board[r][c]){
+         return false
+     }
+     
+     const temp = board[r][c];
+     board[r][c] = "\0"
+     const resp = helper(r+1,c,k+1) || helper(r-1,c,k+1) || helper(r,c+1,k+1) || helper(r,c-1,k+1);
+     board[r][c] = temp
+     return resp;
+    }
+    for(let i = 0;i<row;i++){
+     for(let j = 0;j<col;j++){
+         if(helper(i,j,0)){
+             return true;
+         }
+     }
+    }
+    return false;
+ };
