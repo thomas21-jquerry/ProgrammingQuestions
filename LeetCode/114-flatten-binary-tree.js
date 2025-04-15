0443372308b47c5f1c558743f3762181ed39ecae
thomas21-jquerry <thomasjose875@gmail.com>
2025-04-15 18:35:08 +0530

    done

---
+++ LeetCode/114-flatten-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+ var flatten = function(root) {
+    let cur = root;
+    while(cur){
+        if(cur.left){
+            let runner  = cur.left
+            while(runner.right) runner = runner.right;
+            runner.right = cur.right;
+            cur.right = cur.left;
+            cur.left = null;
+        }
+        cur = cur.right;
+    }
+};
